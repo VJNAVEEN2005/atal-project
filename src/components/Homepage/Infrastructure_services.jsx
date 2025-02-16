1f08d5dae63d99e71c3e2da749980b87eb7001e1
Naveen VJ <vjnaveen2005@gmail.com>
2025-02-16 11:38:41 +0530

    Refactor typography styles in Infrastructure Services component for improved readability and consistency

--- src/components/Homepage/Infrastructure_services.jsx
+++ src/components/Homepage/Infrastructure_services.jsx
@@ -11,57 +11,57 @@ const Infrastructure_services = () => {
         <div className=' flex mt-10'>
             <div className=' w-[50%] flex flex-col items-center justify-center'>
                 <div className=' flex flex-col items-end'>
-                <div className=' font-bold text-3xl'>PLUG & PLAY</div>
-                <div className=' font-bold text-3xl'>Co-Work Space</div>
+                <div className=' font-bold text-2xl'>PLUG & PLAY</div>
+                <div className=' font-bold text-2xl'>Co-Work Space</div>
                 </div>
             </div>
             <div className='w-[50%] flex justify-end'>
-                <div className='pr-10'>
+                <div className='pr-44'>
                 <div className=' w-80 h-52 bg-customBlue rounded-r-full'></div>
                 </div>
             </div>
         </div>
         <div className=' flex mt-5'>
             <div className='w-[50%] flex justify-start'>
-                <div className='pl-10'>
+                <div className='pl-44'>
                 <div className=' w-80 h-52 bg-customBlue rounded-l-full'></div>
                 </div>
             </div>
             <div className=' w-[50%] flex flex-col items-center justify-center'>
                 <div className='flex flex-col items-start'>
-                <div className=' font-bold text-3xl'><li>24/7 High Speed Internet Connectivity.</li></div>
-                <div className=' font-bold text-3xl'><li>World Class Amenities</li></div>
+                <div className=' font-semibold text-2xl'><li>24/7 High Speed Internet Connectivity.</li></div>
+                <div className=' font-semibold text-2xl'><li>World Class Amenities</li></div>
                 </div>
             </div>
         </div>
         <div className=' flex mt-5'>
             <div className=' w-[50%] flex flex-col items-center justify-center'>
                 <div className=' flex flex-col items-end'>
-                <div className=' font-bold text-3xl'>AIC-PEC FOUNDATION</div>
-                <div className=' font-bold text-3xl'>INFRASTRUCTURE SERVICES</div>
-                <div className=' font-bold text-2xl text-customBlue'>Rapid Prototype & Re-engineering</div>
+                <div className=' font-bold text-2xl'>AIC-PEC FOUNDATION</div>
+                <div className=' font-bold text-2xl'>INFRASTRUCTURE SERVICES</div>
+                <div className=' font-bold text-xl text-customBlue'>Rapid Prototype & Re-engineering</div>
                 </div>
             </div>
             <div className='w-[50%] flex justify-end'>
-                <div className='pr-10'>
+                <div className='pr-44'>
                 <div className=' w-80 h-52 bg-customBlue rounded-r-full'></div>
                 </div>
             </div>
         </div>
         <div className=' flex mt-5'>
             <div className='w-[50%] flex justify-start'>
-                <div className='pl-10'>
+                <div className='pl-44'>
                 <div className=' w-80 h-52 bg-customBlue rounded-l-full'></div>
                 </div>
             </div>
             <div className=' w-[50%] flex flex-col items-center justify-center'>
                 <div className='flex flex-col items-start'>
-                <div className=' font-bold text-3xl'>State-of-the-Art Rapid Prototyping Lab</div>
-                <div className=' font-bold text-3xl'><li>Additive Manufactuing Line.</li></div>
-                <div className=' font-bold text-3xl'><li>Internet of Things (IoT).</li></div>
-                <div className=' font-bold text-3xl'><li>Electronic Design Manufacturing (EMD).</li></div>
-                <div className=' font-bold text-3xl'><li>PCB Fabrication Line.</li></div>
-                <div className=' font-bold text-3xl'><li>Unmanned Aerial Vehicle (UAV) Simulation.</li></div>
+                <div className=' font-bold text-2xl'>State-of-the-Art Rapid Prototyping Lab</div>
+                <div className=' font-semibold text-2xl'><li>Additive Manufactuing Line.</li></div>
+                <div className=' font-semibold text-2xl'><li>Internet of Things (IoT).</li></div>
+                <div className=' font-semibold text-2xl'><li>Electronic Design Manufacturing (EMD).</li></div>
+                <div className=' font-semibold text-2xl'><li>PCB Fabrication Line.</li></div>
+                <div className=' font-semibold text-2xl'><li>Unmanned Aerial Vehicle (UAV) Simulation.</li></div>
                 </div>
             </div>
         </div>
