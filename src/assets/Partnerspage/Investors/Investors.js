8f8260a7d4be254caf34a85287dae49bcb312f2b
Pavithra-Djanaguiraman <djpavithra2005@gmail.com>
2025-02-23 14:55:13 +0530

    Added IP and Investors info

---
+++ src/assets/Partnerspage/Investors/Investors.js
@@ -0,0 +1,6 @@
+import amits from './amitsingal.jpg';
+import anil from './Anil Joshi.jpg';
+import puru from './Puru Modani.jpg';
+import rohit from './Rohit.jpg';
+import manoj from './Manoj kumar.jpeg';
+export {amits, anil, puru, rohit, manoj};
